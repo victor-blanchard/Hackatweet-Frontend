a405081a71e24526bd5951044c5d7e2076551fc1
victor-blanchard <159810800+victor-blanchard@users.noreply.github.com>
2024-11-22 15:01:42 +0100

    modif champs de saisie

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,15 +1,31 @@
 import styles from "../styles/Tweet.module.css";
 
+import React from "react";
+import "antd/dist/reset.css";
+import { Button } from "antd";
+import { Input } from "antd";
+import { Flex } from "antd";
+import { Textarea } from "antd";
+
 function Tweet() {
+  const Textarea = Input;
   return (
-    <div>
-      <h1 className={styles.titreTweet}>Home</h1>
-      <textarea className={styles.tweetCreate} placeholder="What's up?" cols="30" rows="3"></textarea>
-      <div className={styles.counterSection}>
-        <div className={styles.charCounter}></div>
-        <button className={styles.tweetButton}></button>
+    <>
+      <div className={styles.AllContainer}>
+        <h1 className={styles.titreTweet}>Home</h1>
+        <div className={styles.TextButton}>
+          <Textarea
+            showCount
+            maxLength={280}
+            className={styles.tweetCreate}
+            placeholder="What's up?"
+            cols="30"
+            rows="3"
+          ></Textarea>
+          <Button className={styles.tweetButton}>Tweet</Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
